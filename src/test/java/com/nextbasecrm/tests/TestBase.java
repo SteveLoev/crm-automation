78a3f78d4216da8c22e5464d3720f1567ac7e134
Steve Loev <steve.loev@gmail.com>
2022-02-26 11:52:29 -0500

    log in function created

--- src/test/java/com/nextbasecrm/tests/TestBase.java
+++ src/test/java/com/nextbasecrm/tests/TestBase.java
@@ -1,2 +1,46 @@
-package com.nextbasecrm.tests;public class TestBase {
+package com.nextbasecrm.tests;
+
+import com.nextbasecrm.utilities.BrowserUtils;
+import com.nextbasecrm.utilities.ConfigurationReader;
+import com.nextbasecrm.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty("browser"));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(ConfigurationReader.getProperty("env"));
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        BrowserUtils.sleep(3);
+        driver.close();
+    }
+
+    @Test
+    public void login_with_valid_credentials_with_login_btn(){
+
+        WebElement userName = driver.findElement(By.xpath("//input[@class='login-inp'][1]"));
+        userName.sendKeys(ConfigurationReader.getProperty("username"));
+
+        WebElement password = driver.findElement(By.xpath("//input[@class='login-inp'][2]"));
+        password.sendKeys(ConfigurationReader.getProperty("password"));
+
+        WebElement loginBtn = driver.findElement(By.xpath("//input[@type='submit']"));
+        BrowserUtils.sleep(2);
+        loginBtn.click();
+    }
 }
